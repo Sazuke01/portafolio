1999da4cdef7df34d2892689c7a06cfd003642f5
Sazuke01 <juancarlos19defebrerohat@gmail.com>
2026-02-11 16:08:11 -0600

    vcv

---
+++ pages/src/main.js
@@ -0,0 +1,12 @@
+function toggleSubMenu(button) {
+  button.nextElementSibling.classList.toggle('show');
+  button.classList.toggle('rotate')
+}
+
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+}
