dd942df8b5ef374d0f376bc72d41519266d22a56
Sazuke01 <juancarlos19defebrerohat@gmail.com>
2026-02-12 00:16:33 -0600

    gfg

--- pages/src/main.js
+++ pages/src/main.js
@@ -1,12 +1,34 @@
-function toggleSubMenu(button) {
-  button.nextElementSibling.classList.toggle('show');
-  button.classList.toggle('rotate')
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function closeAllSubMenus() {
+  Array.from(sidebar.getElementsByClassName('show')).forEach((ul) => {
+    ul.classList.remove('show');
+    ul.previousElementSibling.classList.remove('rotate');
+  });
 }
 
-const toggleButton = document.getElementById('toggle-btn')
-const sidebar = document.getElementById('sidebar')
+function toggleSidebar() {
+  console.log('Precionar boton del sidebar');
 
-function toggleSidebar(){
-  sidebar.classList.toggle('close')
-  toggleButton.classList.toggle('rotate')
+  sidebar.classList.toggle('close');
+  toggleButton.classList.toggle('rotate');
+
+  closeAllSubMenus();
+}
+
+function toggleSubMenu(button) {
+  console.log('Precionar boton del sidebar para el sub menu');
+
+  if (!button.nextElementSibling.classList.contains('show')) {
+    closeAllSubMenus();
+  }
+
+  button.nextElementSibling.classList.toggle('show');
+  button.classList.toggle('rotate');
+
+  if (sidebar.classList.contains('close')) {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+  }
 }
